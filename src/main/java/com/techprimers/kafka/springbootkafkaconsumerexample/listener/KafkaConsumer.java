c94aa921a3c72ad2d0df1e3c9c04952a9b3d6849
Shashank022 <shashank022@gmail.com>
2020-12-21 12:07:08 -0600

    first commit

---
+++ src/main/java/com/techprimers/kafka/springbootkafkaconsumerexample/listener/KafkaConsumer.java
@@ -0,0 +1,30 @@
+package com.techprimers.kafka.springbootkafkaconsumerexample.listener;
+
+import com.techprimers.kafka.springbootkafkaconsumerexample.model.Person;
+import com.techprimers.kafka.springbootkafkaconsumerexample.model.User;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.PartitionOffset;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "Kafka_Example", group = "group_id")
+
+    public void consume(String message) {
+        System.out.println("Consumed message: " + message);
+    }
+
+
+   // @KafkaListener(topics = "QPERSON", group = "group_json", containerFactory = "userKafkaListenerFactory")
+    @KafkaListener(
+            topicPartitions = @TopicPartition(topic = "QPERSON",
+                    partitionOffsets = {
+                            @PartitionOffset(partition = "0", initialOffset = "0"),
+                            @PartitionOffset(partition = "3", initialOffset = "0")}),
+            containerFactory = "userKafkaListenerFactory")
+    public void consumeJson(Person person) {
+        System.out.println("Consumed JSON Message: " + person);
+    }
+}
---
+++ src/main/java/com/techprimers/kafka/springbootkafkaconsumerexample/model/Person.java
@@ -0,0 +1,101 @@
+package com.techprimers.kafka.springbootkafkaconsumerexample.model;
+
+public class Person {
+
+    private int id;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String gender;
+    private String address;
+    private String country;
+    private String zipcode;
+    private String timezone;
+
+    public Person(int id, String first_name, String last_name, String email, String gender, String address, String country, String zipcode, String timezone) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.gender = gender;
+        this.address = address;
+        this.country = country;
+        this.zipcode = zipcode;
+        this.timezone = timezone;
+    }
+
+    public Person() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
+}
